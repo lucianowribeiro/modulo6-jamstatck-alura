26040b7456ebe8d6e192134e289040526b53678d
Luciano WRibeiro <lucianowribeiro@gmail.com>
2021-09-03 09:02:50 -0300

    initial config to monorep

---
+++ README.md
@@ -0,0 +1,3 @@
+# modulo6-jamstatck-alura
+
+Project created during jamstatck-alura to learning a litte about monorep ,storybook and typescript.
---
+++ projetos/site/pages/index.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+import Button from '@modulo-comum/components/Button'
+
+const Title = styled.h1`
+  color: red;
+  font-size: 50px;
+`
+
+export default function Home() {
+  return <Title>My page</Title>
+}
