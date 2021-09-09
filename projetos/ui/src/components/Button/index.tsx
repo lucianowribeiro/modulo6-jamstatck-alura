378436decb925d7ad407a0a80a0a01e124d0a647
Luciano W Ribeiro <luciano.ribeiro@visto.global>
2021-09-09 14:17:38 -0300

    Part 2 aula 4 done

--- projetos/ui/src/components/Button/index.tsx
+++ projetos/ui/src/components/Button/index.tsx
@@ -1,6 +1,15 @@
 import React from "react";
-
-export default function Button({ disabled, children }) {
+interface ButtonProps {
+  /**
+   * Prop que desabilita o botao
+   */
+  disabled?:boolean;
+  /**
+   * Prop que passa via children o que deseja renderizar dentro do componente
+   */
+  children: React.ReactNode
+}
+export default function Button({ disabled, children }: ButtonProps) {
   return (
     <button disabled={disabled}>
       {children}
