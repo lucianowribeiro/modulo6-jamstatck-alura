b9f5e9c084b97790bcf13043a26f5524cc3ff1db
Luciano W Ribeiro <luciano.ribeiro@visto.global>
2021-09-10 13:14:35 -0300

    Aula 5 done

--- projetos/ui/src/components/Button/index.tsx
+++ projetos/ui/src/components/Button/index.tsx
@@ -1,18 +1,54 @@
+import get from "lodash/get";
 import React from "react";
+import styled from "styled-components";
+
+const ButtonWrapper = styled.button`
+  border: 0;
+  cursor: pointer;
+  padding: 12px 26px;
+  font-weight: bold;
+  opacity: 1;
+  border-radius: 8px;
+  color: ${({ theme, color, mode }) =>
+    get(theme, `${mode}.${color}.contrastText`)};
+  background: ${({ theme, color, mode }) =>
+    get(theme, `${mode}.${color}.color`)};
+  &:disabled {
+    cursor: not-allowed;
+    opacity: 0.2;
+  }
+`;
 interface ButtonProps {
+  /**
+   * Prop que seleciona o tema padrao do botao
+   */
+  mode?: string;
+  /**
+   * Prop que seleciona que tipo de tema de cor o botao irá ter
+   */
+  color?: string;
   /**
    * Prop que desabilita o botao
    */
-  disabled?:boolean;
+  disabled?: boolean;
   /**
    * Prop que passa via children o que deseja renderizar dentro do componente
    */
-  children: React.ReactNode
+  children: React.ReactNode;
 }
-export default function Button({ disabled, children }: ButtonProps) {
+export default function Button({
+  disabled,
+  children,
+  color,
+  mode,
+}: ButtonProps) {
   return (
-    <button disabled={disabled}>
+    <ButtonWrapper disabled={disabled} color={color} mode={mode}>
       {children}
-    </button>
-  )
+    </ButtonWrapper>
+  );
 }
+Button.defaultProps = {
+  color: "primary.main",
+  mode: "light",
+};
---
+++ projetos/ui/src/theme/index.ts
@@ -0,0 +1,116 @@
+
+const light = {
+  background: {
+    light: {
+      color: '#FFFFFF',
+    },
+    main: {
+      color: '#F2F2F2',
+    },
+  },
+  borders: {
+    main: {
+      color: '#F1F1F1',
+    },
+  },
+  primary: {
+    main: {
+      color: '#D7385E',
+      contrastText: '#fff',
+    },
+  },
+  secondary: {
+    main: {
+      color: '#FB7B6B',
+      contrastText: '#fff',
+    },
+  },
+  tertiary: {
+    main: {
+      color: '#070C0E',
+      contrastText: '#fff',
+    },
+    light: {
+      color: '#88989E',
+      contrastText: '#fff',
+    },
+  },
+  // Feedback colors
+  error: {
+    main: {
+      color: '#dc3545',
+      contrastText: '#fff',
+    },
+  },
+  success: {
+    main: {
+      color: '#28a745',
+      contrastText: '#fff',
+    },
+  },
+};
+const dark = {
+  background: {
+    light: {
+      color: '#000',
+    },
+    main: {
+      color: '#030506',
+    },
+  },
+  borders: {
+    main: {
+      color: '#181F22',
+    },
+  },
+  primary: {
+    main: {
+      color: '#F27895',
+      contrastText: '#fff',
+    },
+  },
+  secondary: {
+    main: {
+      color: '#FFA59A',
+      contrastText: '#000',
+    },
+  },
+  tertiary: {
+    main: {
+      color: '#FFFFFF',
+      contrastText: '#fff',
+    },
+    light: {
+      color: '#D5D5D5',
+      contrastText: '#fff',
+    },
+  },
+  // Feedback colors
+  error: {
+    main: {
+      color: '#EB5C50',
+      contrastText: '#fff',
+    },
+  },
+  success: {
+    main: {
+      color: '#28a745',
+      contrastText: '#fff',
+    },
+  },
+};
+const breakpoints = {
+  xs: 0,
+  sm: 480,
+  md: 768,
+  lg: 992,
+  xl: 1200,
+};
+export default {
+  light,
+  dark,
+  breakpoints,
+  borderRadius: '12px',
+  fontFamily: '\'Rubik\',sans serif',
+  transition: '200ms ease-in-out',
+};
